950ff83e9a1b6259a786c937386ad9a023c7d249
dg015 <bruno.barbosa8@yahoo.com>
2025-01-22 15:33:34 -0500

    Smoke particle working

--- BarbosaBrunoAssigment2/Assets/Scripts/OutOfCoffee.cs
+++ BarbosaBrunoAssigment2/Assets/Scripts/OutOfCoffee.cs
@@ -6,7 +6,8 @@ namespace NodeCanvas.Tasks.Actions {
 
 	public class OutOfCoffee : ActionTask {
 
-
+		public GameObject SmokeParticle;
+		public Transform spawnPoint;
         //Use for initialization. This is called only once in the lifetime of the task.
         //Return null if init was successfull. Return an error string otherwise
         protected override string OnInit() {
@@ -17,7 +18,8 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			
+			GameObject Particle = Object.Instantiate(SmokeParticle, spawnPoint);
+			EndAction(true);
 		}
 
 		//Called once per frame while the action is active.
